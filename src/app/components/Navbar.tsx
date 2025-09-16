804749a619d813ac6158b9f40ff9da3db7263766
Cory Campbell <campbellcory2360@gmail.com>
2025-09-16 13:34:46 -0400

    Updated navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,7 +3,10 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { NavigationLinks } from "../common/constants";
-// import logo from "@/assets/logo.svg";
+import logo from "../assets/logo.svg";
+import bell from "../assets/bell.svg";
+import avatar from "../assets/avatar.svg";
+import Image from "next/image";
 import clsx from "clsx";
 
 const Navbar = () => {
@@ -14,14 +17,16 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="hidden lg:flex flex-row h-16 w-full border-b-1 border-b-gray-300 justify-between items-center sticky top-0">
+    <nav className="hidden lg:flex flex-row h-16 mb-2 w-full border-b-1 border-b-gray-300 justify-between items-center sticky top-0">
       <div className="w-auto flex">
-        {/* <img src={logo} /> */}
-        <div>
-          <h1>FinTrack</h1>
-        </div>
+        <Link href={`/Dashboard`}>
+          <div className="ml-4 flex flex-row items-center">
+            <Image src={logo} alt={"Logo"} width={50} />
+            <h1 className="ml-2 text-[25px]">BudgetTracker</h1>
+          </div>
+        </Link>
       </div>
-      <div className="w-100 flex flex-row justify-center">
+      <div className="w-auto flex flex-row justify-center">
         {getNavLinks().map((item) => {
           return (
             <div
@@ -41,8 +46,14 @@ const Navbar = () => {
         })}
       </div>
       <div className="w-auto flex justify-end-safe">
-        <div className="flex mr-10">test</div>
-        <div className="flex mr-10">test</div>
+        <div className="flex mr-5 border rounded-4xl p-2 bg-emerald-400">
+          <Image src={bell} alt={"Logo"} width={25} />
+        </div>
+        <div className="flex mr-10 border rounded-4xl p-2">
+          <Link href={`/Account`}>
+            <Image src={avatar} alt={"Logo"} width={25} />
+          </Link>
+        </div>
       </div>
     </nav>
   );
