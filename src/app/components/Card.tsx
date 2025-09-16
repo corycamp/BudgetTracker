1eff9311f9938b1aee0ff0adbe5163f8e695c30a
Cory Campbell <corycampbell@Corys-MacBook-Air.local>
2025-09-16 16:48:59 -0400

    Added shadow

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Card = (props: CardProps) => {
   const { imageSrc, title, amount } = props;
   return (
-    <div className="bg-white flex flex-col mb-5 md:w-16/50 h-40 border-1 border-gray-300 rounded-xl p-4 justify-center">
+    <div className="bg-white flex flex-col mb-5 md:w-16/50 h-40 border-1 border-gray-300 rounded-xl p-4 justify-center shadow-sm">
       <div className="flex flex-row items-center w-full mb-5">
         <div className="border-1 rounded-4xl p-2">
           <Image src={imageSrc} alt={"Card Icon"} width={30} />
