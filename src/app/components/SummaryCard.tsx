1f93a56359a05279652960cf763ca37823e54257
Cory <campbellcory2360@gmail.com>
2025-10-02 19:31:00 -0400

    removed overflow

--- src/app/components/SummaryCard.tsx
+++ src/app/components/SummaryCard.tsx
@@ -7,7 +7,7 @@ import chevronUp from "../assets/chevronUp.svg"
 const SummaryCard = () => {
   const [open,setOpen] = React.useState<boolean>(true);
   return (
-    <div className={`flex flex-col mb-10 p-10 lg:w-49/100 bg-white border-1 border-gray-300 rounded-4xl max-h-155 ${!open && "h-30"} overflow-auto`}>
+    <div className={`flex flex-col mb-10 p-10 lg:w-49/100 bg-white border-1 border-gray-300 rounded-4xl max-h-155 ${!open && "h-30 overflow-hidden"} overflow-auto`}>
       <div className="flex flex-row w-full justify-between items-start">
       <h1 className={`text-[30px] font-bold ${open ? "mb-10" : "mb-0"}`}>Budget Summary</h1>
       <button onClick={()=>setOpen(!open)}>
